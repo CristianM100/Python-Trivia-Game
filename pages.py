5908097ef0b6b2d23149ccf1a01d7caf77f50fec
Cristian Marinescu <cristian.marinescu92@yahoo.com>
2025-10-21 22:55:10 +0200

    create pages.py

---
+++ pages.py
@@ -0,0 +1,7 @@
+from PyQt6.QtWidgets import QGridLayout, QLabel, QPushButton
+from PyQt6.QtGui import QPixmap, QCursor
+from PyQt6 import QtCore
+from urllib.request import urlopen
+import json
+import pandas as pd
+import random
