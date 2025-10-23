26e2955a845362fb72ff783391d6760f8c856618
Cristian Marinescu <cristian.marinescu92@yahoo.com>
2025-10-23 23:42:23 +0200

    crete button in the main page and clear widgets function

--- pages.py
+++ pages.py
@@ -5,3 +5,75 @@ from urllib.request import urlopen
 import json
 import pandas as pd
 import random
+
+# this file contains the code for each window/frame
+
+
+widgets = { 
+    "button": []
+}
+
+grid = QGridLayout()    # initialize grid layout
+
+# hide existing widgets and remove them
+def clear_widgets():
+    for widget in widgets:
+        if widgets[widget] != []:
+            widgets[widget][-1].hide()
+        for i in range(0, len(widgets[widget])):
+            widgets[widget].pop()
+
+def create_buttons(answer, l_margin, r_margin):
+    #create identical buttons with custom left & right margins
+    button = QPushButton(answer)
+    button.setCursor(QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+    button.setFixedWidth(485)
+    button.setStyleSheet(
+        #setting variable margins
+        "*{margin-left: " + str(l_margin) +"px;"+
+        "margin-right: " + str(r_margin) +"px;"+
+        '''
+        border: 4px solid '#BC006C';
+        color: white;
+        font-family: 'shanti';
+        font-size: 16px;
+        border-radius: 25px;
+        padding: 15px 0;
+        margin-top: 20px;
+        }
+        *:hover{
+            background: '#BC006C';
+        }
+        '''
+    )
+    #button.clicked.connect(lambda x: is_correct(button))
+    return button
+
+    #def is_correct(btn):
+    
+
+
+#   FRAME 1  
+
+def frame1():
+    clear_widgets()
+    button = QPushButton("PLAY NOW")
+    button.setCursor(QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+    button.setStyleSheet(
+         '''
+        *{
+            border: 4px solid '#262124';
+            border-radius: 40px;
+            font-size: 30px;
+            color: 'white';
+            padding: 25px 0;
+            margin: 100px 200px;
+        }
+        *:hover{
+            background: '#383537';
+        }
+        '''
+    )
+    widgets["button"].append(button)
+
+    grid.addWidget(widgets["button"][-1], 3, 0, 1, 2)
