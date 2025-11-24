606b54a5a24c7a3e83292918a93ef3b5bf3facb0
Cristian Marinescu <97915515+CristianM100@users.noreply.github.com>
2025-11-24 22:12:07 +0100

    Create main_v7.py

---
+++ main_v7.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+from frames_v5 import frame1, grid
+
+app = QApplication(sys.argv)
+
+# Get screen information
+screen = app.primaryScreen()
+screen_rect = screen.availableGeometry()
+
+# Create window
+window = QWidget()
+window.setWindowTitle("Python Trivia Game")
+
+#window.setFixedHeight(750)
+#window.setFixedWidth(700)
+# Set window to 80% of screen size, centered
+''''
+width = int(screen_rect.width() * 0.8)
+height = int(screen_rect.height() * 0.8)
+x = (screen_rect.width() - width) // 2
+y = (screen_rect.height() - height) // 2
+
+window.setGeometry(x, y, width, height)
+'''
+
+window.setStyleSheet("background: #397591")
+
+
+frame1()
+
+window.setLayout(grid)
+
+window.show()
+app.exec()
